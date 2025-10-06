ce5500d49fb83a9b4a6e452732493f472f996f49
namoo36 <wldnrkwhr36@naver.com>
2025-10-06 18:28:48 +0900

    2025-10-06

---
+++ Board-Server-Locust/AddPosts.py
@@ -0,0 +1,20 @@
+from locust import HttpUser, task, between
+
+import random
+
+class AddPosts(HttpUser):
+    waite_item = between(1, 2)
+
+    def on_start(self):
+        self.client.post("/users/sign-in", json={"userId": "testuser",
+                                                    "password": "123"
+                                                })
+    
+    @task
+    def add_post(self):
+        self.client.post("/posts", json={
+            "name": "테스트 게시글" + str(random.randint(1, 100000)),
+            "contents": "테스트 컨텐츠" + str(random.randint(1, 100000)),
+            "categoryId": random.randint(1, 10),
+            "fileId": random.randint(1, 10)
+        })
